4d7531c16a6ce330f810e3b1b44ca9aad650b4f5
mancoruber1981 <mensajeriadelivery.sas@gmail.com>
2025-08-09 21:23:42 -0500

    Fix API URL in api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 // frontend/src/api/api.js 
  import axios from 'axios'; 
  const API = axios.create({ 
-     baseURL: 'http://127.0.0.1:5000/api', 
+     baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', 
      headers: { 
          'Content-Type': 'application/json', 
      }, 
