e5e9c3cf212486ee78cfd007c1131e3aa1980648
mancoruber1981 <mensajeriadelivery.sas@gmail.com>
2025-09-29 21:15:44 -0500

    fix: Configura la URL del API para producción

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,27 +1,21 @@
-// frontend/src/api/api.js
+// frontend/src/api/api.js (VERSIÓN FINAL PARA PRODUCCIÓN)
 
 import axios from 'axios';
 
-// Creamos la instancia de axios SIN baseURL para que el proxy de package.json funcione
+// La aplicación leerá la URL del backend desde una variable de entorno
 const API = axios.create({
-  baseURL: 'http://localhost:5000' // <-- O el puerto donde esté corriendo TU backend (5000, 5001, etc.)
+  baseURL: process.env.REACT_APP_API_URL
 });
- 
 
+// Interceptor para añadir el token JWT a cada solicitud saliente.
+API.interceptors.request.use((config) => { 
+    const token = localStorage.getItem('token'); 
+    if (token) { 
+        config.headers.Authorization = `Bearer ${token}`; 
+    } 
+    return config; 
+}, (error) => { 
+    return Promise.reject(error); 
+}); 
 
- // Interceptor para añadir el token JWT a cada solicitud saliente. 
- // Esto asegura que todas las peticiones a la API estén autenticadas 
- // si hay un token almacenado en localStorage. 
- API.interceptors.request.use((config) => { 
-     const token = localStorage.getItem('token'); 
-     if (token) { 
-         // Añade el token en el formato "Bearer <token>" al header de Autorización. 
-         config.headers.Authorization = `Bearer ${token}`; 
-     } 
-     return config; 
- }, (error) => { 
-     // Si hay un error en la configuración de la solicitud, se rechaza la promesa. 
-     return Promise.reject(error); 
- }); 
-
- export default API;
+export default API;
